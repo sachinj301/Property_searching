dc3e9fa606dc20c5383ef99865c9f796d9c3ab40
sachin <jjjadhavsssttt@gmail.com>
2021-07-28 19:13:29 +0530

    added media file setting, added records in listing and realtors

--- property/property/settings.py
+++ property/property/settings.py
@@ -123,6 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'property/static')]
