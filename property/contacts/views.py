cd3ea37032f4c4cf3f09b0a4f00b6b18d0bd42f0
sachin <jjjadhavsssttt@gmail.com>
2021-07-29 21:01:22 +0530

    added contact app

--- property/contacts/views.py
+++ property/contacts/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+from .models import Contact
+from django.contrib import messages
+from django.core.mail import send_mail
+
+def contact(request):
+    if request.method == "POST":
+        listing_id = request.POST['listing_id']
+        listing = request.POST['listing']
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        message = request.POST['message']
+        user_id = request.POST['user_id']
+        realtor_email = request.POST['realtor_email']
+
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
+            if has_contacted:
+                messages.error(request, "You have already made inquiry for this Property")
+                return redirect('/listings/'+listing_id)
+        contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone,
+                          message=message, user_id=user_id)
+        contact.save()
+        send_mail('Property Listing Inquiry', 'There has been an inquiry for' + listing +
+                  '.sign into admin panel for more info','jjjadhavsssttt@gmail.com', ['sachintj.301@gmail.com'],
+                  fail_silently=False)
+        messages.success(request, 'Your request has been submitted, a realtor will get back to you soon.')
+        return redirect('/listings/'+listing_id)
