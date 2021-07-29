2d902ebf18c424c43332fe51f5d52540e04773b0
sachin <jjjadhavsssttt@gmail.com>
2021-07-29 21:04:55 +0530

    made changes in index view

--- property/property/urls.py
+++ property/property/urls.py
@@ -22,5 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('search.urls')),
     path('listings/', include('listings.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('contacts/', include('contacts.urls')),
 
          ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- property/search/views.py
+++ property/search/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
 # Create your views here.
+from listings.models import Listing
+from realtors.models import Realtor
+from listings.choices import bedroom_choices, price_choices, state_choices
 
 def index(request):
-    return render(request, 'pages/index.html')
+    list = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+    context = {
+        'listings': list,
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+    }
+    return render(request, 'pages/index.html', context)
 
 def about(request):
-    return render(request, 'pages/about.html')
+    realtor = Realtor.objects.order_by('-hire_date')
+    mvp_realtor = Realtor.objects.all().filter(is_mvp=True)
+    context = {
+        'realtors': realtor,
+        'mvp_realtor': mvp_realtor
+    }
+    return render(request, 'pages/about.html', context)
